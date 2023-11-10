0d4e24f60a90420f18d8cd8c2489e6ee604439e9
Vahe Sevachyan <vscode13@gmail.com>
2023-11-09 17:51:57 -0800

    adding project files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import Hero from "./Components/Hero";
+import Services from "./Components/Services";
+import Tours from "./Components/Tours";
+import Footer from "./Components/Footer";
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <Services />
+      <Tours />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
