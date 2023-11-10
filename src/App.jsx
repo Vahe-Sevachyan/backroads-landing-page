517508a8b2191f3491fd77a8b8cadd72d5c6cdc6
Vahe Sevachyan <vscode13@gmail.com>
2023-11-09 19:26:24 -0800

    updated about component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,14 @@ import Hero from "./Components/Hero";
 import Services from "./Components/Services";
 import Tours from "./Components/Tours";
 import Footer from "./Components/Footer";
+import About from "./Components/About";
 function App() {
   return (
     <>
       <NavBar />
+
       <Hero />
+      <About />
       <Services />
       <Tours />
       <Footer />
