6e56252b38f81ba41f07c18e68d73623ae671098
Vahe Sevachyan <vscode13@gmail.com>
2023-11-11 16:22:30 -0800

    updated tours component with map method iteration to display cards

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,3 +1,9 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+import tour5 from "./images/tour-5.jpeg";
+import tour6 from "./images/tour-6.jpeg";
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#about", text: "about" },
@@ -26,5 +32,74 @@ export const services = [
     icon: "fas fa-socks fa-fw",
     title: "amazing comfort",
     text: " Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "August 26th 2020",
+    title: "Tibet Adventure",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 6,
+    location: 2100,
+    cost: 1599,
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "February 26th 2021",
+    title: "Austria Adventure",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 5,
+    location: 1100,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "March 16th 2022",
+    title: "New York Adventure",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 4,
+    location: 2300,
+    cost: 1399,
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "December 16th 2022",
+    title: "Africa Adventure",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 4,
+    location: 1100,
+    cost: 1299,
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "April 23th 2023",
+    title: "Safari Adventure",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 7,
+    location: 3100,
+    cost: 1899,
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "April 18th 2024",
+    title: "Italy",
+    info: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia. ",
+    location: "",
+    duration: 5,
+    location: 1200,
+    cost: 1999,
   },
 ];
